2b1861d724c8a995219391653d1d9e4eec2b5b76
eatingNumbers <deanlaurea@icloud.com>
2017-04-10 08:13:46 -0500

    Begin Ch.4

---
+++ Ch.4/ascii.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+
+    for (int i = 33; i < 126; i++)
+        printf("%c is %i\n", i, i);
+}
---
+++ Ch.4/float.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    int x = 7;
+    int y = 2;
+    
+    // Cast the integers and turn them into floats.
+    float z = (float) x / (float) y;
+    printf("z = %f\n", z);
+}
